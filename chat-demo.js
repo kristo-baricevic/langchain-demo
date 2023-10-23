23f269ee4d2712e1cf5f4159c1c1dc1e3ecec859
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-23 13:39:59 -0400

    added generate

--- chat-demo.js
+++ chat-demo.js
@@ -1,5 +1,5 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { HumanMessage } from "langchain/schema";
+import { HumanMessage, SystemMessage } from "langchain/schema";
 
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -9,10 +9,34 @@ const model = new ChatOpenAI({
     modelName: "gpt-3.5-turbo",
 });
 
-const res = await  model.call([
-    new HumanMessage(
-        "Translate this sentence from English to Afrikaans. I love programming."
-        ),
-]);
+// const res = await model.call([
+//     new SystemMessage(
+//         "You are a helpful assistant that translates English to Afrikaans."
+//     ),
+//     new HumanMessage(
+//         "Translate this sentence from English to Afrikaans. I love programming."
+//     ),
+// ]);
 
-console.log(res);
+const res = await  model.generate(
+    [
+        [
+            new SystemMessage(
+                "You are a helpful assistant that translates English to Afrikaans."
+            ),
+            new HumanMessage(
+                "Translate this sentence from English to Afrikaans. I love programming."
+            ),
+        ],
+        [
+            new SystemMessage(
+                "You are a helpful assistant that translates English to French."
+            ),
+            new HumanMessage(
+                "Translate this sentence from English to French. I love programming."
+            ),
+        ],
+    ]
+);
+
+console.log(res.generations);
