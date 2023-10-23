ba279f78fc7e89a004623fec31c301f2ec6e7f80
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-23 11:55:24 -0400

    prompt working

--- demo.js
+++ demo.js
@@ -1,8 +1,11 @@
 import {OpenAI } from 'langchain/llms/openai';
-
-require('dotenv').config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const model = new OpenAI({
-    openAIApiKey: "",
     temperature: 0.9,
-})
+});
+
+const res = await model.call("What would be a good name for a company that makes colorful socks?");
+
+console.log(res);
